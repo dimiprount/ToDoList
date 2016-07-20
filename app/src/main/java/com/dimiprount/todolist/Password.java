db093cd851831f849722f69a1b06c196440b8a4d
dimiprount <dimiprount@gmail.com>
2016-07-20 13:43:00 +0300

    Update Password.java

--- app/src/main/java/com/dimiprount/todolist/Password.java
+++ app/src/main/java/com/dimiprount/todolist/Password.java
@@ -3,22 +3,18 @@ package com.dimiprount.todolist;
 public class Password {
 	String pass;
 
-	// Empty constructor
 	public Password() {
 
 	}
 
-	// constructor
 	public Password(String password) {
 		this.pass = password;
 	}
 
-	// getting name
 	public String getPass() {
 		return this.pass;
 	}
 
-	// setting name
 	public void setPass(String password) {
 		this.pass = password;
 	}
