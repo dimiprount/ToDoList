dc92cc71c7514e0f8d8f274aab7ef4450562a21e
dimiprount <dimiprount@gmail.com>
2016-07-20 13:42:37 +0300

    Update Notepad.java

--- app/src/main/java/com/dimiprount/todolist/Notepad.java
+++ app/src/main/java/com/dimiprount/todolist/Notepad.java
@@ -1,42 +1,25 @@
 package com.dimiprount.todolist;
 
-// private variables
 public class Notepad {
 	int id;
 	String note;
 
-	// Empty constructor
 	public Notepad() {
 
 	}
 
-	/*// constructor
-	public Notepad(int keyId, String note) {
-		this.id = keyId;
-		this.note = note;
-	}
-
-	// constructor
-	public Notepad(String note) {
-		this.note = note;
-	}*/
-
-	// getting ID
 	public int getID() {
 		return this.id;
 	}
 
-	// setting id
 	public void setID(int keyId) {
 		this.id = keyId;
 	}
 
-	// getting name
 	public String getNote() {
 		return this.note;
 	}
 
-	// setting name
 	public void setNote(String note) {
 		this.note = note;
 	}
